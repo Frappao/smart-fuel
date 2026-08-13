bcc08daddc0667498f66bae9656ecb759ebe7553
Francesco Paolillo <FRANCESCOPAOLILLO2007@GMAIL.COM>
2026-08-13 18:35:55 +0200

    feat: add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,26 @@
+import type { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://rifornio.it/',
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: 'https://rifornio.it/come-funziona',
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://rifornio.it/privacy',
+      changeFrequency: 'yearly',
+      priority: 0.3,
+    },
+    {
+      url: 'https://rifornio.it/cookie',
+      changeFrequency: 'yearly',
+      priority: 0.3,
+    },
+  ]
+}
